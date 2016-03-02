5aa3c892a4bf26400b81a7c17119c377161ea903
Jeffrey Denton <jeffrey.denton@deutschinc.com>
2016-03-02 17:49:17 -0500

    Reorganized and added ability to independently control x and y animation distance

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
-import Snap from 'snapsvg';
-import {clone, map} from 'lodash';
+import Snap              from 'snapsvg';
+import {clone, map}      from 'lodash';
 import {createRandomRGB} from './helpers';
 
 Snap.plugin( function( Snap, Element, Paper, global ) {
@@ -11,6 +11,30 @@ Snap.plugin( function( Snap, Element, Paper, global ) {
 });
 
 export const App = {
+  init() {
+    let {horizontals, verticals} = this.createPoints(20,20,window.innerHeight,window.innerWidth);
+    let svgInt;
+
+    this.s                = new Snap('#svg');
+    this.horizontals      = map(horizontals, clone);
+    this.verticals        = map(verticals, clone);
+    this.prevX            = 0;
+    this.animationAmountX = 8;
+    this.animationAmountY = 26;
+    this.interval         = 1000;
+    this.strokeWidth      = 1;
+    this.lines            = this.drawLines(this.s, horizontals, verticals);
+
+    this.render(this.lines, horizontals, verticals);
+    svgInt = setInterval(() => {
+      this.render(this.lines, horizontals, verticals);
+    }, this.interval);
+
+    window.stopAnimation = function() {
+      clearInterval(svgInt);
+    };
+  },
+
   createPoints( horizontals, verticals, totalWidth, totalHeight) {
     let width = totalWidth/horizontals;
     let height = totalHeight/verticals;
@@ -46,9 +70,10 @@ export const App = {
     for (let i = 1; i < (newHoriz.length -1); i++) {
       let horizontal = newHoriz[i];
       for (let j = 1; j < (horizontal.length -1); j++) {
-        let factor = Math.floor(Math.random() < 0.5 ? -this.animationAmount : this.animationAmount);
-        let randX  = Math.floor(Math.random() * factor);
-        let randY  = Math.floor(Math.random() * factor);
+        let factorX = Math.floor(Math.random() < 0.5 ? -this.animationAmountX : this.animationAmountX);
+        let factorY = Math.floor(Math.random() < 0.5 ? -this.animationAmountY : this.animationAmountY);
+        let randX  = Math.floor(Math.random() * factorX);
+        let randY  = Math.floor(Math.random() * factorY);
         let x = horizontal[j][0] + randX;
         let y = horizontal[j][1] + randY;
 
@@ -70,8 +95,8 @@ export const App = {
       let line = svg.polyline([].concat.apply([], horizontals[i]));
       line.attr({
         id: `horizontal${i}`,
-        stroke: createRandomRGB(Math.random()/2),
-        strokeWidth: '1',
+        stroke: createRandomRGB(0.25),
+        strokeWidth: this.strokeWidth,
         fill: 'transparent'
       });
       vertLines.push(line);
@@ -81,12 +106,13 @@ export const App = {
       let line = svg.polyline([].concat.apply([], verticals[i]));
       line.attr({
         id: `vertical${i}`,
-        stroke: createRandomRGB(Math.random()/2),
-        strokeWidth: '1',
+        stroke: createRandomRGB(0.25),
+        strokeWidth: this.strokeWidth,
         fill: 'transparent'
       });
       horizLines.push(line);
     }
+
     return {
       vertLines: vertLines,
       horizLines: horizLines
@@ -100,35 +126,13 @@ export const App = {
     let newVert         = newLines.vertLines;
 
     for (let i = 1; i < horizontalLines.length -1; i++) {
-      verticalLines[i].polyAnimate([].concat.apply([], newHoriz[i]), 1000, mina.linear);
-      horizontalLines[i].polyAnimate([].concat.apply([], newVert[i]), 1000, mina.linear);
+      verticalLines[i].polyAnimate([].concat.apply([], newHoriz[i]), this.interval, mina.linear);
+      horizontalLines[i].polyAnimate([].concat.apply([], newVert[i]), this.interval, mina.linear);
     }
   },
 
   render(lines, horizontals, verticals) {
     let newLines = this.updateValues(horizontals, verticals);
     this.animateLines(this.lines, newLines);
-  },
-
-  init() {
-    this.s = new Snap('#svg');
-    let {horizontals, verticals} = this.createPoints(50,50,window.innerHeight,window.innerWidth);
-
-    this.horizontals = map(horizontals, clone);
-    this.verticals   = map(verticals, clone);
-    this.prevX = 0;
-    this.animationAmount = 10;
-
-    this.lines = this.drawLines(this.s, horizontals, verticals);
-
-    let svgInt = setInterval(() => {
-      this.render(this.lines, horizontals, verticals);
-    }, 1000);
-
-    window.stopAnimation = function() {
-      clearInterval(svgInt);
-    };
-
-
   }
 }
